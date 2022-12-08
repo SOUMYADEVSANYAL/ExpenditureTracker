647f6f6fa7bee8142fbeadad97c2080dd3b77cf5
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-12-08 23:37:17 +0530

    New expenses form added.

---
+++ src/components/NewExpenses/NewExpenseForm.js
@@ -0,0 +1,25 @@
+import "./NewExpenseForm.css";
+
+export default function NewExpenseForm() {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2018-01-01" max="2023-01-01" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  );
+}
---
+++ src/components/NewExpenses/NewExpenses.js
@@ -0,0 +1,10 @@
+import NewExpenseForm from './NewExpenseForm';
+import './NewExpense.css';
+
+export default function NewExpenses(){
+    return (
+        <div className='new-expense'>
+            <NewExpenseForm />
+        </div>
+    );
+}
