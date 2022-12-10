1bf0d7800fcbfa623b040e6506c22ad9ebe2fe3a
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-12-10 23:58:53 +0530

    Dynamically ExpenseItem rendered based on dummy array.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,28 +6,16 @@ import ExpenseFilter from "./ExpenseFilter";
 export default function Expenses(props) {
   const expenses = props.expenses;
 
-  function getFilterDateHandler(year){
+  function getFilterDateHandler(year) {
     console.log(year);
   }
 
   return (
     <Card className="expenses">
       <ExpenseFilter selectedYear="2019" getFilterDate={getFilterDateHandler} />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      {props.expenses.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
     </Card>
   );
 }
