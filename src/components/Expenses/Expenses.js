9fcda3e47f3cbc43871825fbd717e4309db5925a
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-12-12 21:11:18 +0530

    Message added for year with no expense.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,20 +6,33 @@ import { useState } from "react";
 
 export default function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2019");
-  
+
   function getFilterDateHandler(year) {
-    
     setFilterYear(year);
   }
 
-  const filteredList = props.expensesList.filter(item => item.date.getFullYear().toString() === filterYear)
+  const filteredList = props.expensesList.filter(
+    (item) => item.date.getFullYear().toString() === filterYear
+  );
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selectedYear={filterYear} getFilterDate={getFilterDateHandler} />
-      {filteredList.map((item) => (
-        <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />
-      ))}
+      <ExpenseFilter
+        selectedYear={filterYear}
+        getFilterDate={getFilterDateHandler}
+      />
+      {filteredList.length === 0 ? (
+        <h1>No expense done in this year.</h1>
+      ) : (
+        filteredList.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))
+      )}
     </Card>
   );
 }
